7a842c845053f58f6186ed66f07ac66e8b798e6b
MohaMed AshRaf <mohmmedashrraf@gmail.com>
2024-10-09 17:13:23 +0300

    p2 رعاية الموظفين - الممارسات الاجتماعية

--- JS/charts.js
+++ JS/charts.js
@@ -78,3 +78,52 @@ genderData.forEach((item, index) => {
         [maleColor, femaleColor]
     );
 });
+
+
+// environment-charts
+const ctx1 = document.getElementById('chart1').getContext('2d');
+const ctx2 = document.getElementById('chart2').getContext('2d');
+
+// رسم المخطط الدائري الأول
+const chart1 = new Chart(ctx1, {
+    type: 'pie',
+    data: {
+        labels: ['الجزء 1', 'الجزء 2', 'الجزء 3', 'الجزء 4'],
+        datasets: [{
+            data: [30, 25, 20, 25],  // نسب
+            backgroundColor: ['#1C639C', '#B3BEC9', '#D7A868', '#B3BEC9'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+// رسم المخطط الحلقي الثاني
+const chart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['الجزء 1', 'الجزء 2', 'الجزء 3', 'الجزء 4'],
+        datasets: [{
+            data: [30, 25, 20, 25],  // نسب
+            backgroundColor: ['#1C639C', '#B3BEC9', '#D7A868', '#B3BEC9'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
